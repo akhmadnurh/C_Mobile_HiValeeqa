dd5fdce408368b27ce64a8b0a9bac266ff1ca7cf
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-07-21 17:06:25 +0700

    Price Format
    module react-number-format

--- global/component.js
+++ global/component.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {StatusBar, Dimensions} from 'react-native';
 import {useIsFocused} from '@react-navigation/core';
+import NumberFormat from 'react-number-format';
 
 export function FocusAwareStatusBar(props) {
   const isFocused = useIsFocused();
 
   return isFocused ? <StatusBar {...props} /> : null;
+}
+
+export function Prices(props) {
+  return (
+    <NumberFormat
+      displayType={'text'}
+      thousandSeparator={true}
+      prefix={'Rp '}
+      {...props}
+    />
+  );
 }
 
 export let deviceWidth = Dimensions.get('window').width;
