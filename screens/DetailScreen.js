5b31a0fcbe6bb7bfdb5338865aff6a9a64f052d5
Akhmad Nur Hidayatulloh <e41191558@student.polije.ac.id>
2021-07-23 04:26:23 +0700

    URL + detail screen bugfix

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,33 +1,33 @@
-import React, {useEffect, useLayoutEffect, useState} from 'react';
-import {View, Text, Image, StyleSheet, Alert, ToastAndroid} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
-import {Button, IconButton} from 'react-native-paper';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import url from '../global/url';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Badge} from 'react-native-elements';
-import {FocusAwareStatusBar, Prices} from '../global/component';
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { View, Text, Image, StyleSheet, Alert, ToastAndroid } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Button, IconButton } from "react-native-paper";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import url from "../global/url";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Badge } from "react-native-elements";
+import { FocusAwareStatusBar, Prices } from "../global/component";
 
-const DetailScreen = ({route, navigation}) => {
-  const {product_id} = route.params;
-  const [product, setProduct] = useState('');
-  const [wishlist, setWishlist] = useState('');
-  const [cart, setCart] = useState('');
-  const [userId, setUserId] = useState('');
+const DetailScreen = ({ route, navigation }) => {
+  const { product_id } = route.params;
+  const [product, setProduct] = useState("");
+  const [wishlist, setWishlist] = useState("");
+  const [cart, setCart] = useState("");
+  const [userId, setUserId] = useState("");
 
   // Get data from api
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       try {
         const checkWishlist = async () => {
-          const user_id = await AsyncStorage.getItem('user_id');
+          const user_id = await AsyncStorage.getItem("user_id");
           setUserId(user_id);
           const data = {
             user_id: user_id,
           };
           axios
-            .get(url + '/api/detail/' + product_id, {
+            .get(url + "/api/detail/" + product_id, {
               params: data,
             })
             .then(res => {
@@ -55,15 +55,15 @@ const DetailScreen = ({route, navigation}) => {
             icon="shopping-outline"
             size={26}
             color="#e87c80"
-            style={{paddingEnd: 0, backgroundColor: '#fff'}}
-            onPress={() => navigation.navigate('Cart', {screen: 'CartScreen'})}
+            style={{ paddingEnd: 0, backgroundColor: "#fff" }}
+            onPress={() => navigation.navigate("Cart", { screen: "CartScreen" })}
           />
           {cart > 0 ? (
             <Badge
               value={cart}
-              badgeStyle={{backgroundColor: '#000'}}
+              badgeStyle={{ backgroundColor: "#000" }}
               containerStyle={{
-                position: 'absolute',
+                position: "absolute",
                 top: 9,
                 right: 1,
               }}
@@ -79,16 +79,16 @@ const DetailScreen = ({route, navigation}) => {
   // Wishlist
   const addWishlist = async () => {
     try {
-      const user_id = await AsyncStorage.getItem('user_id');
+      const user_id = await AsyncStorage.getItem("user_id");
       const data = {
         user_id: user_id,
       };
       axios
-        .get('http://10.0.2.2:8000/api/wishlist/' + product_id, {params: data})
+        .get(url + "/api/wishlist/" + product_id, { params: data })
         .then(res => {
           setWishlist(res.data.status);
           ToastAndroid.showWithGravity(
-            'Berhasil ditambahkan ke Wishlist',
+            "Berhasil ditambahkan ke Wishlist",
             ToastAndroid.SHORT,
             ToastAndroid.TOP,
           );
@@ -100,18 +100,18 @@ const DetailScreen = ({route, navigation}) => {
 
   const removeWishlist = async () => {
     try {
-      const user_id = await AsyncStorage.getItem('user_id');
+      const user_id = await AsyncStorage.getItem("user_id");
       const data = {
         user_id: user_id,
       };
       axios
-        .get('http://10.0.2.2:8000/api/r-wishlist/' + product_id, {
+        .get(url + "/api/r-wishlist/" + product_id, {
           params: data,
         })
         .then(res => {
           setWishlist(res.data.status);
           ToastAndroid.showWithGravity(
-            'Berhasil dihapus dari Wishlist',
+            "Berhasil dihapus dari Wishlist",
             ToastAndroid.SHORT,
             ToastAndroid.TOP,
           );
@@ -127,20 +127,20 @@ const DetailScreen = ({route, navigation}) => {
         user_id: userId,
       };
       axios
-        .get(url + '/api/add-to-cart/' + product_id, {params: data})
+        .get(url + "/api/add-to-cart/" + product_id, { params: data })
         .then(res => {
-          if (res.data.msg == 'success') {
-            axios.get(url + '/api/wishlist', {params: data}).then(res => {
+          if (res.data.msg == "success") {
+            axios.get(url + "/api/wishlist", { params: data }).then(res => {
               setCart(res.data.data.cart);
             });
             ToastAndroid.showWithGravity(
-              'Produk berhasil ditambahkan ke Keranjang Belanja',
+              "Produk berhasil ditambahkan ke Keranjang Belanja",
               ToastAndroid.SHORT,
               ToastAndroid.TOP,
             );
           } else {
             ToastAndroid.showWithGravity(
-              'Produk gagal ditambahkan ke Keranjang Belanja',
+              "Produk gagal ditambahkan ke Keranjang Belanja",
               ToastAndroid.SHORT,
               ToastAndroid.TOP,
             );
@@ -152,28 +152,28 @@ const DetailScreen = ({route, navigation}) => {
   };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <FocusAwareStatusBar barStyle="dark-content" backgroundColor="#fff" />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.img}>
           <Image
             resizeMode="cover"
-            style={{width: 200, height: 300}}
-            source={{uri: 'http://10.0.2.2:8000/img/produk/' + product.image}}
+            style={{ width: 200, height: 300 }}
+            source={{ uri: url + "/img/produk/" + product.image }}
           />
         </View>
         <View style={styles.title}>
           <Prices
             value={product.price}
             renderText={value => (
-              <Text style={{fontSize: 24, fontWeight: '700'}}>{value}</Text>
+              <Text style={{ fontSize: 24, fontWeight: "700" }}>{value}</Text>
             )}
           />
-          <Text style={{fontSize: 16, fontWeight: '600', marginTop: 4}}>
-            {product.product_name}{' '}
+          <Text style={{ fontSize: 16, fontWeight: "600", marginTop: 4 }}>
+            {product.product_name}{" "}
             {product.stock < 1 ? (
               <Text>
-                |<Text style={{color: '#dc3545'}}> Stok produk kosong</Text>
+                |<Text style={{ color: "#dc3545" }}> Stok produk kosong</Text>
               </Text>
             ) : (
               <View></View>
@@ -181,7 +181,7 @@ const DetailScreen = ({route, navigation}) => {
           </Text>
         </View>
         <View style={styles.description}>
-          <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 16}}>
+          <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 16 }}>
             Detail Produk
           </Text>
           <View style={styles.list}>
@@ -207,8 +207,8 @@ const DetailScreen = ({route, navigation}) => {
             disabled
             mode="contained"
             color="#e87c80"
-            labelStyle={{color: '#fff'}}
-            style={{borderRadius: 8, flex: 0.8, elevation: 0}}
+            labelStyle={{ color: "#fff" }}
+            style={{ borderRadius: 8, flex: 0.8, elevation: 0 }}
             onPress={addToCart}>
             Add to Cart
           </Button>
@@ -216,8 +216,8 @@ const DetailScreen = ({route, navigation}) => {
           <Button
             mode="contained"
             color="#e87c80"
-            labelStyle={{color: '#fff'}}
-            style={{borderRadius: 8, flex: 0.8, elevation: 0}}
+            labelStyle={{ color: "#fff" }}
+            style={{ borderRadius: 8, flex: 0.8, elevation: 0 }}
             onPress={addToCart}>
             Add to Cart
           </Button>
@@ -229,8 +229,8 @@ const DetailScreen = ({route, navigation}) => {
             style={{
               borderRadius: 8,
               marginStart: 8,
-              justifyContent: 'center',
-              borderColor: '#FF8195',
+              justifyContent: "center",
+              borderColor: "#FF8195",
             }}
             onPress={addWishlist}>
             <Icon name="heart-outline" size={24} />
@@ -242,8 +242,8 @@ const DetailScreen = ({route, navigation}) => {
             style={{
               borderRadius: 8,
               marginStart: 8,
-              justifyContent: 'center',
-              borderColor: '#e87c80',
+              justifyContent: "center",
+              borderColor: "#e87c80",
             }}
             onPress={removeWishlist}>
             <Icon name="heart" size={24} />
@@ -256,25 +256,25 @@ const DetailScreen = ({route, navigation}) => {
 
 const styles = StyleSheet.create({
   img: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     height: 300,
-    alignItems: 'center',
+    alignItems: "center",
   },
   list: {
     paddingVertical: 12,
     paddingHorizontal: 8,
     borderBottomWidth: 1,
-    borderBottomColor: '#eee',
-    flexDirection: 'row',
+    borderBottomColor: "#eee",
+    flexDirection: "row",
   },
   listTitle: {
     marginRight: 40,
   },
   listText: {
-    color: '#6c757d',
+    color: "#6c757d",
   },
   title: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingVertical: 24,
     paddingHorizontal: 16,
     marginVertical: 1,
@@ -285,16 +285,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 4,
   },
   description: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginTop: 4,
     paddingVertical: 16,
     paddingHorizontal: 16,
   },
   containerBtn: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
     paddingVertical: 12,
     elevation: 5,
   },
